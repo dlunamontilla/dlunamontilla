05efbbab207b500c98e85f6e9135fc2d209cf47e
David E Luna M <davidlunamontilla@gmail.com>
2022-06-07 09:30:46 -0500

    Se elimina la función «saveFormDataToServer», porque ya no es necesaria para el envío de peticiones a través del formulario

--- assets/send-form.js
+++ assets/send-form.js
@@ -8,37 +8,6 @@ const isForm = (any) => {
 };
 
 /**
- * Permite guardatos los datos del formulario en el servidor.
- *
- * @param { HTMLFormElement } form Ingrese un formulario como parámetro
- * @param { string } nameRegister Si va a almacenar datos del formulario
- * en localStorage solo debe especificar una clave o item.
- *
- * @returns { Promise<any> }
- */
-async function saveFormDataToServer(form, nameRegister = "") {
-  /** @type { boolean } */
-  const localData = nameRegister.trim().length > 0;
-
-  if (!form) return;
-
-  const formData = getFormData(form);
-  const { action, method } = form;
-
-  const response = await fetch(action, {
-    method,
-    body: JSON.stringify(formData),
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-  });
-
-  if (localData) saveFormData("locations", formData);
-
-  return response;
-}
-
-/**
  * Permite enviar el formulario mediante una petición Ajax.
  *
  * @param { HTMLFormElement } form Formulario HTML
