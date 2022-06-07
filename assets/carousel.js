4b18db0bc6c2d35a20679862a5413de61e65ce57
David E Luna M <davidlunamontilla@gmail.com>
2022-06-07 18:04:28 -0500

    Últimas actualizaciones

--- assets/carousel.js
+++ assets/carousel.js
@@ -11,13 +11,12 @@ const carousel = (selector) => {
    */
   const sliderContainers = document.querySelectorAll(selector);
 
+  console.clear();
   sliderContainers.forEach((sliderContainer) => {
-    const images = sliderContainer.querySelectorAll("[data-name='images']");
+    const images = sliderContainer.querySelectorAll('[data-name="images"]');
     const indicators = sliderContainer.querySelectorAll(
       "[data-name='indicators']"
     );
-
-    console.log({ images, indicators });
 
     /**
      * @type { number } Images and indicators index
@@ -56,16 +55,52 @@ const carousel = (selector) => {
       const { name } = element.dataset;
       if (!name || name !== "carousel") return;
 
-      console.log("Esto es antes de visualizar el mensaje");
-
       //   Si no está contenido o no es una función no continuará
       if (!(name in direction) || !(typeof direction[name] !== "function")) {
         return;
       }
-
-      console.log("Se ve este mensaje significa que funciona");
     });
   });
 };
 
-carousel("[data-name='carousel']");
+/**
+ * Get SVG files and debug them before inserting into HTML
+ * @returns { void }
+ */
+const renderArrow = () => {
+  const elements = document.querySelectorAll("[data-svg*='.svg']");
+
+  console.log({ elements });
+
+  elements.forEach(async (element) => {
+    const { svg } = element.dataset;
+    if (!svg) return;
+
+    const response = await fetch(`${svg}`);
+    if (!response.ok) {
+      console.error(`No se pudo cargar ${svg}`);
+      return;
+    }
+
+    const html = sanitizeText(await response.text());
+
+    // Insert SVG to element HTML:
+    element.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+/**
+ *
+ * @param { string } text Find all the script inject in the code before parsing.
+ * @returns { string }
+ */
+const sanitizeText = (text) => {
+  /**
+   * @type { RegExp }
+   */
+  const findScript = /(<script.*|<\/[a-z]+pt>)/gim;
+  return text.replace(findScript, "");
+};
+
+renderArrow();
+carousel('[data-name="carousel"]');
