51468ea7e1c47d01600440761d7d829fb788a4de
David E Luna M <davidlunamontilla@gmail.com>
2022-06-13 10:34:42 -0500

    Se toman los datos del carrito de compra

--- assets/minicart.js
+++ assets/minicart.js
@@ -1,1 +1,18 @@
 // Todo el código del minicart acá.
+
+(function () {
+  const quantityFields = document.querySelectorAll("[name='quantity']");
+
+  /**
+   * Truncar cualquier caracter que no sea numérico:
+   */
+  quantityFields.forEach((quantity) => {
+    quantity.addEventListener("keydown", function (e) {
+      const key = e.key;
+
+      if (isNaN(key) && key !== "Backspace") {
+          e.preventDefault();
+      }
+    });
+  });
+})();
